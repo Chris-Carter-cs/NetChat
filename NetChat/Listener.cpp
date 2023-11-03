8d712571983755eaea733d6f331f5b5a8fb1fbb4
chris-carter-cs <Chris.carter.cs@gmail.com>
2023-11-02 20:51:19 -0500

    Created listener thread.

---
+++ NetChat/Listener.cpp
@@ -0,0 +1,68 @@
+#include "Listener.h"
+
+int Listener::listener_main() {
+	runLoop = 1;
+	initSocket();
+
+	startListening();
+
+	return 0;
+}
+
+
+/// <summary>
+/// Initilize the socket for this componenet.
+/// </summary>
+int Listener::initSocket() {
+	int result = WSAStartup(MAKEWORD(2, 2), &data);
+	if (result != 0) {
+		printf("Listening socket failed with error %d\n", result);
+		return 1;
+	}
+
+	listenSocket = INVALID_SOCKET;
+	listenSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (listenSocket == INVALID_SOCKET) {
+		printf("Listening socket failed with error %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	/*if (listenPort == 0)*/ listenPort = 5850;
+
+	ZeroMemory(&listenAddr, sizeof(listenAddr));
+	listenAddr.sin_family = AF_INET;
+	listenAddr.sin_port = htons(listenPort);
+	listenAddr.sin_addr.s_addr = INADDR_ANY;
+	//inet_pton(AF_INET, "127.0.0.1", &listenAddr.sin_addr.s_addr);
+
+	if (bind(listenSocket, (const struct sockaddr*)&listenAddr, sizeof(listenAddr)) < 0) {
+		printf("Bind failed with error %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	printf("UDP Listener set up. Ready for call to recv\n");
+
+	return 0;
+}
+
+/// <summary>
+/// Begin listening for a message from another user. This function is blocking, as it calls recv(...)
+/// </summary>
+int Listener::startListening() {
+	//Clear buffer.
+	ZeroMemory(buffer, BUFFER_SIZE);
+	int result;
+	while (runLoop && (result = recv(listenSocket, buffer, BUFFER_SIZE, 0)) > 0);
+	{
+		if (result < 0) {
+			printf("Error in startListening function of type %d\n", WSAGetLastError());
+			runLoop = 0;
+			return 1;
+		}
+		//Process data...
+		printf("\tLength %d Datagram received: %s\n", result, buffer);
+	}
+
+	
+	return 0;
+}
---
+++ NetChat/Listener.h
@@ -0,0 +1,28 @@
+#pragma once
+//#include <winsock.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+
+
+class Listener
+{
+	static const int DEFAULT_PORT = 5850;
+	static const int BUFFER_SIZE = 2048;
+
+	struct sockaddr_in listenAddr;
+	SOCKET listenSocket;
+	WSAData data;
+	short listenPort;
+
+	char buffer[2048];
+
+public:
+	int runLoop;
+
+	int listener_main();
+	int initSocket();
+	int startListening();
+};
+
